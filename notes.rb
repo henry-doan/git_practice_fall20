fbbf89668e1b81097f6d600e350abb69db5e6f44
Henry Doan <henrydoan96@gmail.com>
2020-08-26 19:00:29 -0600

    create files

---
+++ notes.rb
@@ -0,0 +1,43 @@
+GIT
+  - version control system
+  - keep track of your coding version 
+  - 2005 LinusTorvalds
+  - store, collabs 
+  
+Github
+  - all versions of your code 
+  - bitbuckets 
+  - hosting service. 
+  - Ruby on Rails 
+  - 2008 
+  - 2018 ms 7.5 Billion 
+    - 7.5 Billion minute  14,269 years 
+    - $100,000 / year 75,000 years 
+    - Canada $208.33
+
+
+brew install git 
+ssh 
+
+Repositories
+- all projects will be a repository
+
+Warning* 
+- system directory as a repo (Desktop, Docs, Application)
+- never create a repo in another repo 
+- delete .git 
+  rm -rf .git 
+
+
+git init 
+- create a repo in the current folder you are in
+- run once pre project 
+
+git status 
+- info coding version process
+
+.gitignore
+- anything in the file will be ignored
+
+git add .
+- track these changes 
